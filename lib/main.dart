da457f0ade49c9237defdaf2995ab7096e5637f8
oscar calle <ocalle374@gmail.com>
2024-06-29 18:02:44 -0400

    need to add a column to organize the widgets

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: const Center(
+        body: Center(
           child: TabWidgets(),
+          // Images(),
         ),
+        Container(),
       ),
     );
   }
@@ -26,10 +28,16 @@ class Images extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(
-        child: Image(
-          image: AssetImage('assets/ezgif.com-webp-to-png-converter.png'),
-        ),
-      ),
+          child: Image(
+              image: NetworkImage(
+                  'https://static.wixstatic.com/media/6f02ae_e0aa6e0e88f141009c6d087d6bb07f3b~mv2.png/v1/fill/w_267,h_297,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/365_logo_Red_Wht_transparentBG.png'))
+          // child: Image(
+          // image: AssetImage('assets/ezgif.com-webp-to-png-converter.png'),
+
+          // width: 267,
+          // height: 297,
+          // fit: BoxFit.contain,
+          ),
     );
   }
 }
